963d676f27977e1ae8878bc4f6367888aa2e62d3
Vojtech Malek <malek.vojta2@gmail.com>
2025-03-13 17:39:54 +0100

    added comments to mqqt-client.py

--- robobot/mqtt_python/mqtt-client.py
+++ robobot/mqtt_python/mqtt-client.py
@@ -23,14 +23,12 @@
 #* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #* THE SOFTWARE. */
 
-#import sys
-#import threading
 import time as t
-#import select
 import numpy as np
 import cv2 as cv
 from datetime import *
 from setproctitle import setproctitle
+
 # robot function
 from spose import pose
 from sir import ir
@@ -40,6 +38,7 @@ from sedge import edge
 from sgpio import gpio
 from scam import cam
 from uservice import service
+from ulog import flog
 
 # set title of process, so that it is not just called Python
 setproctitle("mqtt-client")
@@ -71,8 +70,8 @@ def imageAnalysis(save):
 
 ############################################################
 
-stateTime = datetime.now()
-def stateTimePassed():
+stateTime = datetime.now() # define globally in mqqt-client.py
+def stateTimePassed(): # how much time has passed since last state change
   return (datetime.now() - stateTime).total_seconds()
 
 ############################################################
@@ -97,7 +96,7 @@ def driveOneMeter():
     else:
       print(f"# drive 1m drove {pose.tripB:.3f}m in {pose.tripBtimePassed():.3f} seconds")
       service.send("robobot/cmd/ti/rc","0.0 0.0") # (forward m/s, turn-rate rad/sec)
-      break;
+      break
     print(f"# drive {state}, now {pose.tripB:.3f}m in {pose.tripBtimePassed():.3f} seconds")
     t.sleep(0.05)
   pass
@@ -126,7 +125,7 @@ def driveTurnPi():
     else:
       print(f"# drive turned {pose.tripBh:.3f} rad in {pose.tripBtimePassed():.3f} seconds")
       service.send("robobot/cmd/ti/rc","0.0 0.0") # (forward m/s, turn-rate rad/sec)
-      break;
+      break
     print(f"# turn {state}, now {pose.tripBh:.3f} rad in {pose.tripBtimePassed():.3f} seconds")
     t.sleep(0.05)
   pass
@@ -136,27 +135,31 @@ def driveTurnPi():
 ############################################################
 
 def loop():
-  from ulog import flog
-  state = 0
-  images = 0
-  ledon = True
-  tripTime = datetime.now()
-  oldstate = -1
-  startTime = t.time() # temporary for testing
+  state = 0 # current state
+  images = 0 # number of images taken
+  ledon = True # LED on/off
+  oldstate = -1 # previous state
+  startTime = t.time() # time since beginning of mission
+
   service.send(service.topicCmd + "T0/leds","16 30 30 0") # LED 16: yellow - waiting
-  if service.args.meter:
-    state = 101 # run 1m
-  elif service.args.pi:
-    state = 102 # run 1m
-  elif not service.args.now:
+
+  if service.args.meter: # if it was run with --meter
+    state = 101 # driveOneMeter
+  elif service.args.pi: # if it was run with --pi
+    state = 102 # driveTurnPi
+  elif not service.args.now: # if not run with --now -> wait for green start button
     print("% Ready, press start button")
+
+  edge.lineControl(0.0, 0) # make sure line control is off
+
   # main state machine
-  edge.lineControl(0.1, 0) # make sure line control is off
-  while not (service.stop or gpio.stop()):
-    if t.time() - startTime >= 20:  # Check if 5 seconds have passed
+  while not (service.stop or gpio.stop()): # main loop (until red stop button is pressed, or stop signal received)
+
+    if t.time() - startTime >= 20:  # time limit for mission
       print("% Mission finished due to time limit")
       break
-    if state == 0: # wait for start signal
+
+    if state == 0: # starting state (waiting for start signal or --now)
       start = gpio.start() or service.args.now
       if start:
         print("% Starting")
@@ -165,19 +168,21 @@ def loop():
         # follow line (at 0.25cm/s)
         edge.lineControl(0.25, 0.0) # m/s and position on line -2.0..2.0
         state = 110 # until no more line
-        pose.tripBreset() # use trip counter/timer B
+        pose.tripBreset()
+
     elif state == 12: # following line
-      if edge.lineValidCnt == 0 or pose.tripBtimePassed() > 10:
-        # no more line
+      if edge.lineValidCnt == 0 or pose.tripBtimePassed() > 10: # no line or following for too long
         edge.lineControl(0,0) # stop following line
         pose.tripBreset()
         service.send(service.topicCmd + "ti/rc","0.1 0.5") # turn left
         state = 14 # turn left
+        
     elif state == 14: # turning left
       if pose.tripBh > np.pi/2 or pose.tripBtimePassed() > 10:
         state = 20 # finished   =17 go look for line
         service.send(service.topicCmd + "ti/rc","0 0") # stop for images
       print(f"% --- state {state}, h = {pose.tripBh:.4f}, t={pose.tripBtimePassed():.3f}")
+
     elif state == 20: # image analysis
       imageAnalysis(images == 2)
       images += 1
@@ -195,10 +200,11 @@ def loop():
         state = 99
       pass
 
-    # testing states
+    ###### TESTING STATES #######
     elif state == 101:
       driveOneMeter()
       state = 100
+
     elif state == 102:
       driveTurnPi()
       state = 100
@@ -219,6 +225,7 @@ def loop():
     else: # abort
       print(f"% Mission finished/aborted; state={state}")
       break
+
     # allow openCV to handle imshow (if in use)
     # images are almost useless while turning, but
     # used here to illustrate some image processing (painting)
@@ -228,8 +235,6 @@ def loop():
       if key > 0: # e.g. Esc (key=27) pressed with focus on image
         break
 
-    edge.plot_error() # plot error
-
     # note state change and reset state timer
     if state != oldstate:
       flog.write(state)
@@ -237,10 +242,14 @@ def loop():
       print(f"% State change from {oldstate} to {state}")
       oldstate = state
       stateTime = datetime.now()
+
     # do not loop too fast
     t.sleep(0.1)
+    
     pass # end of while loop
+
   # end of mission, turn LEDs off and stop
+  edge.plot_error() # plot error (temporary for PID tuning)
   service.send(service.topicCmd + "T0/leds","16 0 0 0") 
   gpio.set_value(20, 0)
   edge.lineControl(0,0) # stop following line
